801d75e2ddd373a27ab4b8fc4002228a3a315aaf
anilece <47322405+anilece@users.noreply.github.com>
2019-05-06 12:07:08 +0530

    Create PRODUCT_RULE_7.py

---
+++ PRODUCT_RULE_7.py
@@ -0,0 +1,18 @@
+#PF-Assgn-15
+
+def find_product(num1,num2,num3):
+    product=0
+    if (num1==7):
+        product=num2*num3
+    elif(num2==7):
+        product=num3
+    elif(num3==7):
+        product=-1
+    else:
+        product=num1*num2*num3
+    #write your logic here
+    return product
+
+
+product=find_product(7,6,2)
+print(product)
