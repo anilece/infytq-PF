8fa976405aa61d126c5fa3a5ffd5953bba007a4d
anilece <47322405+anilece@users.noreply.github.com>
2019-05-06 12:08:07 +0530

    Create GRADE_PERCENT.py

---
+++ GRADE_PERCENT.py
@@ -0,0 +1,12 @@
+
+percent=80
+if (percent>=73 and percent<=79):
+    print('B')
+elif (percent>=80 and percent<=100):
+    print('A')
+elif(percent>=65 and percent<=72):
+    print('C')
+elif(percent>=0 and percent<=64):
+    print('D')
+else:
+    print('Z')
