f86909bfc41d7bef61f0917336be67826e881124
Anil_Kumar_R <47322405+anilece@users.noreply.github.com>
2019-05-15 14:49:40 +0530

    Create FLAVOUR_TOPPINGS_D7.py

---
+++ FLAVOUR_TOPPINGS_D7.py
@@ -0,0 +1,12 @@
+#PF-Exer-34
+
+def find_number_of_combination(number_of_flavours):
+    total_combination=0
+    total_combination=2**number_of_flavours
+    
+    return total_combination
+
+
+
+number_of_combination=find_number_of_combination(6)
+print(number_of_combination)
