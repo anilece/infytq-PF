2822caf545bd9bc3c36c6d6a91908ff3cb66e4e8
Anil_Kumar_R <47322405+anilece@users.noreply.github.com>
2019-05-15 14:53:14 +0530

    Create LAMBDA_E38.py

---
+++ LAMBDA_E38.py
@@ -0,0 +1,19 @@
+#PF-Exer-38
+#This verification is based on string match.
+import math
+num1=36
+num2=7
+num3=18
+
+calc =lambda a,b:(a%b)+(a-b) 
+
+print(calc(num1,num2))
+
+square_root =lambda a:math.sqrt(a)
+    
+
+print(square_root(num3))
+
+square_root2=lambda a: a**(1/2)
+
+print(square_root2(num3))
