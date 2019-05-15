3fb3dea69b066b4d00ec0001ab998e44e388723b
Anil_Kumar_R <47322405+anilece@users.noreply.github.com>
2019-05-15 14:54:17 +0530

    Create SI_LAMBDA_FUNC.py

---
+++ SI_LAMBDA_FUNC.py
@@ -0,0 +1,15 @@
+#PF-Exer-39
+#This verification is based on string match.
+
+principal_amount=4000
+duration=12
+rate_of_interest=13
+
+simple_interest = lambda principal_amount,duration,rate_of_interest:((principal_amount*duration*rate_of_interest)/100)
+
+
+if(simple_interest(principal_amount,duration,rate_of_interest)>1000):
+    
+    print("Platinum Member")
+else:
+    print("Gold Member")
